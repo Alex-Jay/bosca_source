6c87875ac55819603f58996cd4e33bf2dd3b485b
Alex-Jay <alexjaycontact@gmail.com>
2019-02-04 22:15:16 +0000

    Add console and command listener

---
+++ source/Assets/GConsole/Scripts/Commands/GConsoleQuit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Adds the quit command to the console, which closes the application (if in non-web non-editor build).
+/// </summary>
+public class GConsoleQuit : MonoBehaviour {
+
+	void Start () {
+	    GConsole.AddCommand("quit", "Quits the application.", QuitApplication);
+	}
+
+    string QuitApplication(string param)
+    {
+            Application.Quit();
+            return null; //No point in returning a message if the application has already shut down.
+    }
+}
